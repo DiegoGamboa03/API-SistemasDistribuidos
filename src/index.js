27527189cb439f8a92470b6799db3ce4b75626b7
Diego Gamboa <diego.gamboaa03@gmail.com>
2022-10-20 09:53:41 -0400

    Organizacion de carpetas y conexion a la bbdd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const db = require('./Config/DatabaseConfig');
 
 
 app.set('port', process.env.port || 3000)
