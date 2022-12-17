e868c5156f6afb056aa71b69a05b586661eb485c
Karen Morán <kamoran.19@est.ucab.edu.ve>
2022-12-17 11:30:19 -0400

    Ruta Device_rules

--- src/Routes/Device_rules.js
+++ src/Routes/Device_rules.js
@@ -2,3 +2,68 @@ const { Router } = require('express');
 const router = new Router();
 const conn = require('../Config/DatabaseConfig');
 
+router.get('/', (req, res) => {
+    const sql = 'SELECT * FROM Device_rules';
+
+    conn.query(sql, (error, results) => {
+    if (error){
+      if(error.errno == 1054) {
+        res.statusCode = 202; 
+        res.send('No rules found');
+        return;
+      }
+      res.statusCode = 500; //meter un status que tenga aqui
+      res.send(error.sqlMessage);
+      return;
+    }
+    if (results.length > 0) {
+      res.json(results);
+    }
+    });
+});
+
+router.post('/add', (req, res) => {
+    const sql = 'INSERT INTO Device_rules SET ?';
+  
+    const ruleObj = {
+      ID_fact: req.body.fact,
+      ID_device: req.body.device,
+      ID_topic: req.body.topic
+    };
+    // Aqui poner las verificaciones
+    
+    conn.query(sql, ruleObj, error => {
+        if (error){
+          if(error.errno == 1054) {
+            res.statusCode = 202; 
+            res.send('No rule found');
+            return;
+          }
+          res.statusCode = 500;
+          res.send(error.sqlMessage);
+          return;
+        }
+        res.send('Device rule created!');
+    });
+});
+
+router.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = `DELETE FROM Device_rules WHERE ID = "${id}"`;
+  
+    conn.query(sql, error => {
+        if (error){
+          if(error.errno == 1054) {
+            res.statusCode = 202; 
+            res.send('No rule found');
+            return;
+          }
+          res.statusCode = 500;
+          res.send(error.sqlMessage);
+          return;
+        }
+        res.send('Rule deleted!');
+    });
+});
+
+module.exports = router;
