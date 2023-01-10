b614d3fd831cfa07ebff6566ae134f77ee80cd20
eliasgit32 <eliasjpb32@gmail.com>
2023-01-09 20:24:11 -0400

    Utils.js acomodado para enviar pisos

--- src/Routes/Utils.js
+++ src/Routes/Utils.js
@@ -5,65 +5,67 @@ const conn = require('../Config/DatabaseConfig');
 
 
 router.get('/getStatus', (req, res) => {
-    const sql = 'SELECT * FROM Rooms';
-    let rooms = [];
-    conn.query(sql, (error, results) => {
-    if (error){
-      if(error.errno == 1054) {
-        res.statusCode = 202; 
-        res.send('No devices found');
+  let floors = [];  
+  //Consulta de pisos
+  const sql = 'SELECT * FROM Floors';
+  conn.query(sql, (error, results) => {
+    if (error) {
+      if (error.errno == 1054) {
+        res.statusCode = 202;
+        res.send('No floors found');
         return;
       }
       res.statusCode = 500;
       res.send(error.sqlMessage);
       return;
-    }else if (results.length > 0) { 
-      for(let i=0; i < results.length; i++){
-        let jsonRoom = {
-          IDRoom:  results[i]['ID'],
-          Devices: []
-        } 
-        
-        rooms.push(jsonRoom);
+    } else if (results.length > 0) {
+      // Llenar arreglo de pisos
+      for (let i = 0; i < results.length; i++) {
+        let jsonFloor = {
+          IDFloor: results[i]['ID'],
+          Rooms: []
+        }
+        floors.push(jsonFloor);
       }
-      
-      const sql2 = 'SELECT * FROM Devices';
-      
+      //Consulta de habitaciones
+      const sql2 = 'SELECT * FROM Rooms';
       conn.query(sql2, (error, results2) => {
-        if (error){
-          if(error.errno == 1054) {
-            res.statusCode = 202; 
-            res.send('No devices found');
+        if (error) {
+          if (error.errno == 1054) {
+            res.statusCode = 202;
+            res.send('No floors found');
             return;
           }
           res.statusCode = 500;
           res.send(error.sqlMessage);
           return;
-        }else if (results2.length > 0) {
-          
-            for(let i=0; i<rooms.length; i++){
-              for (let j = 0; j < results2.length; j++) {
-                if (rooms[i]['IDRoom'] == results2[j]['Room']) {
-                  let jsonDevice = {
-                    IDDevice: results2[j]['ID'],
-                    Type: results2[j]['Type'],
-                    Status: results2[j]['Status'],
-                    Value: results2[j]['Value']
-                  }
-                  //devices.push(jsonDevice);
-                  rooms[i].Devices.push(jsonDevice)
+        } else if (results2.length > 0) {
+          //Llenar arreglo de habitaciones
+          for (let i = 0; i < floors.length; i++) {
+            for (let j = 0; j < results2.length; j++) {
+              if (floors[i]['IDFloor'] == results2[j]['Floor']) {
+                let jsonRoom = {
+                  IDRoom: results2[j]['ID'],
+                  PosX: results2[j]['PosX'],
+                  PosY: results2[j]['PosY'],
+                  Devices: []
                 }
+                floors[i].Rooms.push(jsonRoom)
               }
             }
-            res.send(rooms);            
           }
-        })
-    }else{
+          res.send(floors)
+        } else {
+          return;
+        }
+      })
+      
+    } else {
       res.statusCode = 202;
-      res.send('No room found')
+      res.send('No floor found')
       return;
-    } 
-    });
+    }
+  });
 });
 
 module.exports = router;
